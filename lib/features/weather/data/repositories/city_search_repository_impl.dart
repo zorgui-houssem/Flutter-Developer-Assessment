fac02ea6afd6ecb1b497e30a4d7988850f4c5dc5
Houssem Zorgui <zorguihoussem06@gmail.com>
2026-06-08 13:10:00 +0100

    feat(search): add geocoding database resources and search suggestions

---
+++ lib/features/weather/data/repositories/city_search_repository_impl.dart
@@ -0,0 +1,16 @@
+﻿import 'package:injectable/injectable.dart';
+import '../../domain/entities/city_suggestion.dart';
+import '../../domain/repositories/city_search_repository.dart';
+import '../datasources/city_search_datasource.dart';
+
+@LazySingleton(as: CitySearchRepository)
+class CitySearchRepositoryImpl implements CitySearchRepository {
+  final CitySearchDatasource _datasource;
+
+  const CitySearchRepositoryImpl(this._datasource);
+
+  @override
+  Future<List<CitySuggestion>> searchCities(String query, {int limit = 5}) {
+    return _datasource.searchCities(query, limit: limit);
+  }
+}
---
+++ lib/features/weather/domain/entities/city_suggestion.dart
@@ -0,0 +1,27 @@
+﻿import 'package:equatable/equatable.dart';
+
+class CitySuggestion extends Equatable {
+  final String name;
+  final String country;
+  final String? state;
+  final double lat;
+  final double lon;
+
+  const CitySuggestion({
+    required this.name,
+    required this.country,
+    this.state,
+    required this.lat,
+    required this.lon,
+  });
+
+  String get displayName {
+    final parts = [name];
+    if (state != null && state!.isNotEmpty) parts.add(state!);
+    parts.add(country);
+    return parts.join(', ');
+  }
+
+  @override
+  List<Object?> get props => [name, country, state, lat, lon];
+}

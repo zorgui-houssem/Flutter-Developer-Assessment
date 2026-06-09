737741fe2155df4ce8061d6e646c89fb70e1a4b3
Houssem Zorgui <zorguihoussem06@gmail.com>
2026-06-09 12:30:00 +0100

    design(theme): refine light/dark color palettes and fix light mode city widget contrast

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color darkBackground = Color(0xFF030712);
+
+  static const Color darkBackgroundSecondary = Color(0xFF0B132B);
+
+  static const Color darkCardSurface = Color(0x14FFFFFF);
+
+  static const Color darkAccent = Color(0xFF6366F1);
+
+  static const Color darkTextPrimary = Color(0xFFF8FAFC);
+
+  static const Color darkTextSecondary = Color(0xFF94A3B8);
+
+  static const Color offlineBannerColor = Color(0xFFF97316);
+
+  static const Color errorColor = Color(0xFFEF4444);
+
+  static const Color darkGlassBorder = Color(0x1EFFFFFF);
+
+  static const Color lightBackground = Color(0xFFF8FAFC);
+
+  static const Color lightBackgroundSecondary = Color(0xFFEEF2F6);
+
+  static const Color lightCardSurface = Color(0xF2FFFFFF);
+
+  static const Color lightAccent = Color(0xFF4F46E5);
+
+  static const Color lightTextPrimary = Color(0xFF0F172A);
+
+  static const Color lightTextSecondary = Color(0xFF64748B);
+
+  static const Color lightGlassBorder = Color(0x1E4F46E5);
+
+  static const Color shimmerBaseDark = Color(0xFF1E293B);
+
+  static const Color shimmerHighlightDark = Color(0xFF334155);
+
+  static const Color shimmerBaseLight = Color(0xFFE2E8F0);
+
+  static const Color shimmerHighlightLight = Color(0xFFF1F5F9);
+}
